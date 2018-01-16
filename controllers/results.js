4fb514ee1e989adf4d31dfb1de5c1bd7fe3cc3bf
Eli Wheaton <ewheaton@gmail.com>
2018-01-16 16:02:00 -0500

    Results list

--- controllers/results.js
+++ controllers/results.js
@@ -1,12 +1,14 @@
 const Survey = require('../models').survey;
+const SurveyAnswer = require('../models').surveyAnswer;
 
 // Results main
 exports.index = (req, res, next) => {
   Survey
-    .count()
-    .then((total) => {
-      res.render('results/index', { total });
+    .findAll({
+      include: { model: SurveyAnswer },
+    })
+    .then((surveys) => {
+      res.render('results/index', { surveys });
     })
     .catch(err => next(err));
 };
-
